70407e79957aa97061f567e818e993871537d2ab
a2nfinance <levi@a2n.finance>
2024-05-11 16:02:17 +0700

    update main.py

--- ppl/nn/main.py
+++ ppl/nn/main.py
@@ -6,13 +6,8 @@ from detrain.ppl.args_util import get_args
 from detrain.ppl.worker import run_worker
 from detrain.ppl.dataset_util import get_torchvision_dataset
 from shards_model import NNShard1, NNShard2
-from torch.distributed.optim.optimizer import DistributedOptimizer
 import torch.optim as optim
-# Define model here
-# torchrun \
-# --nproc_per_node=1 --nnodes=3 --node_rank=0 \
-# --master_addr=localhost --master_port=9999 \
-# main.py --use_syn
+
 if __name__=="__main__":
     args = get_args()
     # Get args
@@ -21,16 +16,25 @@ if __name__=="__main__":
     epochs = int(args.epochs)
     batch_size = int(args.batch_size)
     lr = float(args.lr)
-    device = "cpu"
-
-    # Check devices
-    if (args.gpu is not None):
-        device = "cuda"
 
     for i in range(torch.cuda.device_count()):
         print(torch.cuda.get_device_properties(i).name)
-    
 
+    devices = []
+    workers = []
+    shards = [NNShard1, NNShard2]
+    # Check devices
+    if (args.gpu is not None):
+        arr = args.gpu.split('_')
+        for dv in range(len(arr)):
+            if dv > 0:
+                workers.append(f"worker{dv}")
+                if int(arr[dv]) == 1:
+                    devices.append("cuda:0")
+                else:
+                    devices.append("cpu")
+    if (rank > 0):
+        print("Tesla P40")
     # Define optimizer & loss_fn
     loss_fn = nn.CrossEntropyLoss()
     optimizer_class = optim.SGD
@@ -41,6 +45,7 @@ if __name__=="__main__":
 
     
     print(f"World_size: {world_size}, Rank: {rank}")
+    
     num_split = 4
     tik = time.time()
     run_worker(
@@ -48,9 +53,9 @@ if __name__=="__main__":
         world_size, 
         (
             args.split_size, 
-            ["worker1", "worker2"],
-            [device, device], 
-            [NNShard1, NNShard2]
+            workers,
+            devices, 
+            shards
         ), 
         train_dataloader, 
         test_dataloader, 
